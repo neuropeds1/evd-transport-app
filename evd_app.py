0f49382d51acedb0b4a86c1ad56019c669546244
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-18 12:43:03 -0700

    evd_app.py

--- evd_app.py
+++ evd_app.py
@@ -15,16 +15,24 @@ if "inputs" not in st.session_state:
 data = st.session_state.inputs
 def next_step():
     st.session_state.step += 1
-
+# step 1
 if st.session_state.step ==1:
     iht_type = st.radio("Check one of the following:", ["ICP >= 20mmHg", "Escalation of ICP Category Compared to Pre-Transport ICP Category"], key ="iht_type")
     if st.button("Next"):
         st.session_state.inputs["iht_type"] = iht_type
         next_step()
+# step 2 greater than 20
 elif st.session_state.step == 2 and (data['iht_type'] == "ICP >= 20mmHg"):
-    st.title("hi")
+    icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg", "≥20 mmHg"], key="icp_cat")
+    if st.button("Next"):
+        st.session_state.inputs["icp_category"] = icp
+        next_step()
+# step 2 other
 elif st.session_state.step == 2 and (data['iht_type'] == "Escalation of ICP Category Compared to Pre-Transport ICP Category"):
-    st.title("bye")
+   icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg"], key="icp_cat")
+    if st.button("Next"):
+        st.session_state.inputs["icp_category"] = icp
+        next_step()
 
         
     
