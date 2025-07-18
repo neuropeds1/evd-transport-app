ab9d91c8c34958da35713618633f8e1d2b019a81
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-18 13:15:30 -0700

    evd_app.py

--- evd_app.py
+++ evd_app.py
@@ -13,26 +13,23 @@ st.session_state.inputs["iht_type"] = iht_type
 icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg", "≥20 mmHg"], key="icp_cat")
 st.session_state.inputs["icp_category"] = icp
 
-# step 2 other
-#icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg"], key="icp_cat")
-#st.session_state.inputs["icp_category"] = icp
 
-#intubated = st.radio("Is the patient intubated?", ["Yes", "No"], key="intubation")
-#st.session_state.inputs["intubated"] = intubated
+intubated = st.radio("Is the patient intubated?", ["Yes", "No"], key="intubation")
+st.session_state.inputs["intubated"] = intubated
 
 # Step 4: Duration
-#duration = st.slider("Duration of IHT (minutes):", 0, 120, 30, key="duration")
-#st.session_state.inputs["duration"] = duration
+duration = st.slider("Duration of IHT (minutes):", 0, 120, 30, key="duration")
+st.session_state.inputs["duration"] = duration
 
 
 # Step 5: Days since ICU admission
 
-#days = st.slider("Days since ICU admission:", 0, 30, 5, key="icu_days")
+days = st.slider("Days since ICU admission:", 0, 30, 5, key="icu_days")
 
-#st.session_state.inputs["days_since_icu"] = days
+st.session_state.inputs["days_since_icu"] = days
  
 
-# Step 6: CSF Drain
-#csf_drain = st.number_input("Hourly CSF drained (mL/h):", min_value=0.0, value=5.0, key="csf")
+ Step 6: CSF Drain
+csf_drain = st.number_input("Hourly CSF drained (mL/h):", min_value=0.0, value=5.0, key="csf")
 
-#st.session_state.inputs["csf_drain"] = csf_drain
+st.session_state.inputs["csf_drain"] = csf_drain
