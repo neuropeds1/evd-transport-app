7635504aa65d7271342be7d78bd863d9ed4b4cd0
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-18 13:01:12 -0700

    evd_app.py

--- evd_app.py
+++ evd_app.py
@@ -18,24 +18,26 @@ def next_step():
 
 go = True
 # step 
-if st.session_state.step ==1:
-    iht_type = st.radio("Check one of the following:", ["ICP >= 20mmHg", "Escalation of ICP Category Compared to Pre-Transport ICP Category"], key ="iht_type")
-    if st.button("Next"):
-        st.session_state.inputs["iht_type"] = iht_type
-        next_step()
+while go:
+    if st.session_state.step ==1:
+        iht_type = st.radio("Check one of the following:", ["ICP >= 20mmHg", "Escalation of ICP Category Compared to Pre-Transport ICP Category"], key ="iht_type")
+        if st.button("Next"):
+            st.session_state.inputs["iht_type"] = iht_type
+            next_step()
 # step 2 greater than 20
-elif st.session_state.step == 2 and (data['iht_type'] == "ICP >= 20mmHg"):
-    icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg", "≥20 mmHg"], key="icp_cat")
-    if st.button("Next"):
-        st.session_state.inputs["icp_category"] = icp
-        next_step()
-        go = False
+    elif st.session_state.step == 2 and (data['iht_type'] == "ICP >= 20mmHg"):
+        icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg", "≥20 mmHg"], key="icp_cat")
+        if st.button("Next"):
+            st.session_state.inputs["icp_category"] = icp
+            next_step()
+            go = False
 # step 2 other
-elif st.session_state.step == 2 and (data['iht_type'] == "Escalation of ICP Category Compared to Pre-Transport ICP Category"):
-    icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg"], key="icp_cat")
-    if st.button("Next"):
-        st.session_state.inputs["icp_category"] = icp
-        next_step()
+    elif st.session_state.step == 2 and (data['iht_type'] == "Escalation of ICP Category Compared to Pre-Transport ICP Category"):
+        icp = st.radio("Pre-IHT ICP category:", ["<15 mmHg", "15–19 mmHg"], key="icp_cat")
+        if st.button("Next"):
+            st.session_state.inputs["icp_category"] = icp
+            next_step()
+            go = False
 # Step 3: Intubation
 #intubated = st.radio("Is the patient intubated?", ["Yes", "No"], key="intubation")
 #st.session_state.inputs["intubated"] = intubated
