eda96da90c3155da98d5cd1d1542cc0f795907de
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-25 16:58:52 -0700

    evd_app.py

--- evd_app.py
+++ evd_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import math
-from st_pages import Page, show_pages, add_page_tittle
+from st_pages import Page, show_pages, add_page_title
 
 show_pages([Page("streamlit_app.py", "Home", "la"), Page("other_pages/page2.py", "page 2", "la"), ])
