fc9b68413c9c25854d381e7bb4f3d760fa446d9f
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-26 12:49:31 -0700

    evd_app.py

--- evd_app.py
+++ evd_app.py
@@ -100,7 +100,7 @@ if next == True:
     
 
     # Write row to CSV
-    with open("icp_risk_log.csv", mode="a", newline="", encoding = "utf-8") as f:
+    with open(log_file, mode="a", newline="", encoding = "utf-8") as f:
       writer = csv.DictWriter(f, fieldnames=log_data.keys())
     if not file_exists:
       writer.writeheader()
