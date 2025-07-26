2d9174092753173d22f5eab24061cb084259dada
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-26 11:37:42 -0700

    evd_app.py

--- evd_app.py
+++ evd_app.py
@@ -58,9 +58,7 @@ if next == True:
     risk *= risk_iht
     risk *= risk_unscheduled
     risk *= 0.118
-    def round_to_2(y):
-      final_risk = float(format(y, ".2f"))
-    round_to_2(risk)
+    final_risk = round(risk,2)
     st.header(final_risk)
       
     
