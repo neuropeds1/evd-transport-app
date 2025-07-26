ffc3f223c17ef284680dbaf18bba28729aafec59
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-25 17:06:11 -0700

    evd_app.py

--- evd_app.py
+++ evd_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import math
-from st_pages import Page, show_pages, add_page_title
 
-#show_pages([Page("streamlit_app.py", "Home", "la"), Page("other_pages/page2.py", "page 2", "la"), ])
+st.title("Quality & Safety During Intra - Hospital Transport of Patients With a Clamped External Ventricular Drain")
+
+st.header("Calculating the Risk of Intracranial Pressure Elevation During Intra - Hospital Transport")
+
+
+
